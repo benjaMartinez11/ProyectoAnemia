44ed1a11455ee38200fa4fa6873153776b514289
benjamin martinez <benjamartinez454gmail.com>
2025-10-28 20:08:54 -0300

    hice mi primer test

--- example/example.tsx
+++ example/example.tsx
@@ -1,9 +1,7 @@
-import { PropsWithChildren } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { PropsWithChildren } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-export const CustomText = ({ children }: PropsWithChildren) => (
-  <Text>{children}</Text>
-);
+export const CustomText = ({ children }: PropsWithChildren) => <Text>{children}</Text>;
 
 export default function HomeScreen() {
   return (
@@ -16,8 +14,8 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
